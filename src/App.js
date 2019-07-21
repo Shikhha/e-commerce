88057ce05f93e14102b98254d676f624a45a6210
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-21 06:43:09 +0530

    Added product lists

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Products from "./components/Product";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    products: [],
+    filteredProducts: []
+  };
+
+  componentWillMount() {
+    fetch("http://localhost:8000/products/")
+      .then(res => res.json())
+      .then(data => this.setState({ products: data, filteredProducts: data }));
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>Ecommerce shopping cart application</h1>
+        <br />
+        <div className="row">
+          <div className="col-md-8">
+            <Products products={this.state.filteredProducts} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
